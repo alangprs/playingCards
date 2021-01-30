bdc16d617b5f4a6e56c04bafdc9f27a5e506d5e5
will <alangprs1@gmail.com>
2021-01-30 11:44:57 +0800

    修改成 func 有回傳值 + switch

--- playingCards/ViewController.swift
+++ playingCards/ViewController.swift
@@ -17,78 +17,81 @@ class ViewController: UIViewController {
         /*
             利用巢狀迴圈將花色跟數字結合，存進自定義屬性Card裡
          */
-        for cube in 0..<cube.count{ //花色
-            for rank in 0..<rank.count{ //數字
+        for cube in cube{ //花色
+            for rank in rank{ //數字
                 var card = Card()
-                card.Cube = "\(cube)"
-                card.Rank = "\(rank)"
+                card.Cube = cube
+                card.Rank = rank
                 cards.append(card)
             }
         }
-        
+    
+    }
+    
+    //判斷花色func
+    func judgeCubeFunc(cards:Card) -> Int{
+        var cubeNumber = 0
+        switch cards.Cube {
+        case "Plum":
+            cubeNumber = 1
+        case "Cube":
+            cubeNumber = 2
+        case "Love":
+            cubeNumber = 3
+        default:
+            cubeNumber = 4
+        }
+        return cubeNumber
         
     }
+    //判斷數字func
+    func judgeRankFunc(cards:Card) -> Int{
+        var rankNumber = 0
+        if cards.Rank == "1"{ //判斷Ａ是最大的
+            rankNumber = 14
+        }else{
+            rankNumber = Int("\(cards.Rank)")!
+        }
+        return rankNumber
+    }
+
     //開牌按鈕
     @IBAction func again(_ sender: UIButton) {
             //亂數產生圖片
-        cube.shuffle() //亂數花色
-        rank.shuffle() //亂數數字
-        
+        cards.shuffle()
+
         /*
             宣一個常數存電腦、玩家 用迴圈將名字跟數字組合起來的照片名稱
             電腦 = [0] 玩家 = [1]
             */
+        let 電腦 = cards[0]
         //電腦卡片
-        let computerCard = cube[0]+rank[0]
-        imageView[0].image = UIImage(named: "\(computerCard)")
+        imageView[0].image = UIImage(named:"\(電腦.Cube)\(電腦.Rank)")
+        
+        let 玩家 = cards[1]
         //玩家卡片
-        let myCard = cube[1]+rank[1]
-        imageView[1].image = UIImage(named: "\(myCard)")
-        
-        /*
-            花色是string不好比大小，用firstIndex判斷string在array的順序決定大小
-            將A(1) 設為最大數
-            將rank轉為數字方便比大小
-         */
-        //電腦花色
-        let computerCube = cube.firstIndex(of:cube[0])
-        //電腦數字 轉int
-        var computerNumber = 0
-         if rank[0] == "1"{
-             computerNumber = 14
-         }else{
-             computerNumber = Int("\(rank[0])")!
-         }
-        //玩家花色
-        let myCube = cube.firstIndex(of: cube[1])
-        //玩家數字 轉int
-        var myNumber = 0
-        if rank[1] == "1"{
-            myNumber = 14
-        }else{
-            myNumber = Int("\(rank[1])")!
-        }
-        
+        imageView[1].image = UIImage(named: "\(玩家.Cube)\(玩家.Rank)")
        
+        
         /*
           先數字判斷大小
           如果數字一樣，判斷花色來決定誰大誰小
           */
-        if computerNumber == myNumber{
-            
-            if computerCube! > myCube!{
-            resultLabel.text = "電腦贏"
-            }else if computerCube! < myCube!{
-                resultLabel.text = "玩家贏"
-        }
-        }else if computerNumber > myNumber{
-            resultLabel.text = "電腦贏"
+        let 判斷電腦數字 = judgeRankFunc(cards: cards[0])
+        let 判斷玩家數字 = judgeRankFunc(cards: cards[1])
+        
+        if 判斷電腦數字 > 判斷玩家數字{
+            resultLabel.text = "電腦獲勝"
+        }else if 判斷電腦數字 < 判斷玩家數字{
+            resultLabel.text = "玩家獲勝"
         }else{
-            resultLabel.text = "玩家贏"
+            resultLabel.text = "平手拉"
         }
         
-
-    }
-  
+        
+        
+        
+        
 }
 
+}
